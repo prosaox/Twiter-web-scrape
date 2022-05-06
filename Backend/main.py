50600d1276baa1fd3cc4cd1ddd0f628b65ae555a
Hiep Nguyen <nguyen62@myumanitoba.ca>
2022-05-06 18:15:32 -0500

    update 3000/15 min

--- Backend/main.py
+++ Backend/main.py
@@ -10,28 +10,23 @@ auth.set_access_token("1463578925024657410-OYwNQ1hmYLUdhUcHzBm7krz9cjUuAB",
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-name="MikezGarcia"
-# user = api.get_user(screen_name=name)
-# count = user.followers_count
+name="vietnam_idol"
+user = api.get_user(screen_name=name)
+counts = user.followers_count
 # print("User details:")
 # print(user.screen_name)
 # print(user.description)
 # print(user.location)
-# data=[]
-# print(name + " has " + str(count) + " followers.")
-# for follower in tweepy.Cursor(api.get_followers,screen_name=name).items(count):
-#     x=[follower.name,follower.description,follower.location]
-#     data.append(x)
 
-
-c = tweepy.Cursor(api.get_follower_ids, screen_name = name)
+c = tweepy.Cursor(api.get_followers, screen_name = name,count=200)
 ids = []
 for x in c.items():
     ids.append(x)
+    print(len(ids))
 
 with open('results.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(["Name", "Description", "Location"])
     for x in ids:
-        user = api.get_user(user_id=x)
-        writer.writerow([user.screen_name, user.description, user.location])
+        # user = api.get_user(user_id=x)
+        writer.writerow([x.screen_name, x.description, x.location])
