e12ee88cc730d64d86c9fbbef55d6cab3b44f948
Hiep Nguyen <nguyen62@myumanitoba.ca>
2022-05-07 22:11:04 -0500

    fix over capacity bug

--- Backend/updateAlgorithm.py
+++ Backend/updateAlgorithm.py
@@ -11,28 +11,33 @@ auth.set_access_token("1463578925024657410-OYwNQ1hmYLUdhUcHzBm7krz9cjUuAB",
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-name="vietnam_idol"
+name="USDA"
 user = api.get_user(screen_name=name)
 counts = user.followers_count
-
-c = tweepy.Cursor(api.get_follower_ids, screen_name = name,count=5000)
-ids = []
-for x in c.items():
-    ids.append(x)
-print(len(ids))
-count=0
-list=[]
 with open('results.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(["Name", "Description", "Location"])
-    for x in ids:
-        count+=1
-        if count==100:
-            list.append(x)
-            user = api.lookup_users(user_id=list[0:99])
-            for i in range(len(user)):
-                writer.writerow([user[i].screen_name, user[i].description, user[i].location])
-            list=[]
-            count=0
-        else:
-            list.append(x)
+c = tweepy.Cursor(api.get_follower_ids, screen_name = name,count=5000)
+countPhase=1
+ids = []
+for x in c.items():
+    ids.append(x)
+    if len(ids)==75000:
+        count=0
+        list=[]
+        with open('results.csv', 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for x in ids:
+                count+=1
+                if count==100:
+                    list.append(x)
+                    user = api.lookup_users(user_id=list[0:99])
+                    for i in range(len(user)):
+                        writer.writerow([user[i].screen_name, user[i].description, user[i].location])
+                    list=[]
+                    count=0
+                else:
+                    list.append(x)
+        ids=[]
+        print("finish"+str(countPhase*75000))
+        countPhase+=1
